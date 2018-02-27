8fb6baf564f12c90e16d27b9394392f6f255f119
root <root@mail.vvjob.cn>
2018-02-27 14:00:26 +0800

    修正

--- src/tpl/errors/error_exception.tpl.php
+++ src/tpl/errors/error_exception.tpl.php
@@ -2,7 +2,7 @@
 
 <div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
 
-    <h4>程序出了点小问题，您可以返回首页，或稍后再试。</h4>
+    <h4>程序出了点小问题，您可以返回<a href="/">首页</a>，或稍后再试。</h4>
 
     <p>Type: <?php echo get_class($exception); ?></p>
     <p>Message: <?php echo $message; ?></p>
