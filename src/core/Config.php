8aa61cdd7854f636594c4f4ef0f60a1adc76d327
huangshaowen <40477536@qq.com>
2018-12-17 17:39:46 +0800

    配置支持三级

--- src/core/Config.php
+++ src/core/Config.php
@@ -6,19 +6,13 @@ namespace framework\core;
  *
  * 网站配置
  */
-class Config {
+class Config implements \ArrayAccess {
 
     /**
      * 配置参数
      * @var array
      */
     protected $config = [];
-
-    /**
-     * 配置前缀
-     * @var string
-     */
-    protected $prefix = 'app';
 
     /**
      * 构造方法
@@ -36,127 +30,139 @@ class Config {
         return $obj;
     }
 
-    // 设定配置参数的作用域
-    public function range($prefix) {
-        $this->prefix = $prefix;
-        if (!isset($this->config[$prefix])) {
-            $this->config[$prefix] = [];
-        }
-    }
-
     /**
      * 加载配置文件（PHP格式）
      *
      * @param string $file 配置文件名
      * @param string $name 配置名（如设置即表示二级配置）
-     * @param string $prefix  作用域
      * @return mixed
      */
-    public function load($file, $name = '', $prefix = '') {
-        $prefix = $prefix ?: $this->prefix;
-        if (!isset($this->config[$prefix])) {
-            $this->config[$prefix] = [];
-        }
-        /* 如果是文件 */
-        if (is_file($file)) {
-            return $this->set(include $file, $name, $prefix);
-        }
-        /* 如果是文件夹 */
-        if (is_dir($file)) {
-            chdir($file);
-            $configFiles = glob("*.php");
-            foreach ($configFiles as $single_file) {
-                $this->set(include $file . $single_file, $name, $prefix);
+    public function load($file, $name = '') {
+        if (is_file($file) || is_dir($file)) {
+            /* 如果是文件 */
+            if (is_file($file)) {
+                return $this->set(include $file, $name);
+            } elseif (is_dir($file)) {
+                /* 如果是文件夹 */
+                chdir($file);
+                $configFiles = glob("*.php");
+                foreach ($configFiles as $single_file) {
+                    $this->set(include $file . $single_file, $name);
+                }
             }
         }
-        /* 默认处理 */
-        return $this->config[$prefix];
+
+        return $this->config;
     }
 
     /**
      * 检测配置是否存在
-     *
-     * @param string $name 配置参数名（支持二级配置 .号分割）
-     * @param string $prefix  作用域
+     * @access public
+     * @param  string    $name 配置参数名（支持多级配置 .号分割）
      * @return bool
      */
-    public function has($name, $prefix = '') {
-        $prefix = $prefix ?: $this->prefix;
+    public function has($name) {
+        return !is_null($this->get($name));
+    }
+
+    /**
+     * 获取一级配置
+     * @access public
+     * @param  string    $name 一级配置名
+     * @return array
+     */
+    public function pull($name) {
         $name = strtolower($name);
 
-        if (!strpos($name, '.')) {
-            return isset($this->config[$prefix][$name]);
-        } else {
-            // 二维数组设置和获取支持
-            $name = explode('.', $name);
-            return isset($this->config[$prefix][$name[0]][$name[1]]);
-        }
+        return isset($this->config[$name]) ? $this->config[$name] : [];
     }
 
     /**
      * 获取配置参数 为空则获取所有配置
-     *
-     * @param string $name 配置参数名（支持二级配置 .号分割）
-     * @param string $prefix  作用域
+     * @access public
+     * @param  string    $name      配置参数名（支持多级配置 .号分割）
+     * @param  mixed     $default   默认值
      * @return mixed
      */
-    public function get($name = null, $prefix = '') {
-        $prefix = $prefix ?: $this->prefix;
+    public function get($name = null, $default = null) {
         // 无参数时获取所有
-        if (empty($name) && isset($this->config[$prefix])) {
-            return $this->config[$prefix];
+        if (empty($name)) {
+            return $this->config;
         }
+
+        if ('.' == substr($name, -1)) {
+            return $this->pull(substr($name, 0, -1));
+        }
+
         $name = strtolower($name);
-        if (!strpos($name, '.')) {
-            // 判断环境变量
-            if (isset($_ENV[$name])) {
-                return $_ENV[$name];
+        $name = explode('.', $name);
+        $config = $this->config;
+
+        // 按.拆分成多维数组进行判断
+        foreach ($name as $val) {
+            if (isset($config[$val])) {
+                $config = $config[$val];
+            } else {
+                return $default;
             }
-            return isset($this->config[$prefix][$name]) ? $this->config[$prefix][$name] : null;
-        } else {
-            // 二维数组设置和获取支持
-            $name = explode('.', $name);
-            // 判断环境变量
-            if (isset($_ENV[$name[0] . '_' . $name[1]])) {
-                return $_ENV[$name[0] . '_' . $name[1]];
-            }
-            return isset($this->config[$prefix][$name[0]][$name[1]]) ? $this->config[$prefix][$name[0]][$name[1]] : null;
         }
+
+        return $config;
     }
 
     /**
      * 设置配置参数 name为数组则为批量设置
-     *
-     * @param string $name 配置参数名（支持二级配置 .号分割）
-     * @param mixed $value 配置值
-     * @param string $prefix  作用域
+     * @access public
+     * @param  string|array  $name 配置参数名（支持三级配置 .号分割）
+     * @param  mixed         $value 配置值
      * @return mixed
      */
-    public function set($name, $value = null, $prefix = '') {
-        $prefix = $prefix ?: $this->prefix;
-        if (!isset($this->config[$prefix])) {
-            $this->config[$prefix] = [];
-        }
+    public function set($name, $value = null) {
         if (is_string($name)) {
             $name = strtolower($name);
-            if (!strpos($name, '.')) {
-                $this->config[$prefix][$name] = $value;
+            $name = explode('.', $name, 3);
+
+            if (count($name) == 2) {
+                $this->config[strtolower($name[0])][$name[1]] = $value;
             } else {
-                // 二维数组设置和获取支持
-                $name = explode('.', $name);
-                $this->config[$prefix][$name[0]][$name[1]] = $value;
+                $this->config[strtolower($name[0])][$name[1]][$name[2]] = $value;
             }
-            return;
+
+            return $value;
         } elseif (is_array($name)) {
             // 批量设置
             if (!empty($value)) {
-                return $this->config[$prefix][$value] = array_change_key_case($name);
+                if (isset($this->config[$value])) {
+                    $result = array_merge($this->config[$value], array_change_key_case($name));
+                } else {
+                    $result = $name;
+                }
+
+                $this->config[$value] = $result;
             } else {
-                return $this->config[$prefix] = array_merge($this->config[$prefix], array_change_key_case($name));
+                $result = $this->config = array_merge($this->config, array_change_key_case($name));
             }
         } else {
             // 为空直接返回 已有配置
-            return $this->config[$prefix];
+            $result = $this->config;
+        }
+
+        return $result;
+    }
+
+    /**
+     * 移除配置
+     * @access public
+     * @param  string  $name 配置参数名（支持三级配置 .号分割）
+     * @return void
+     */
+    public function remove($name) {
+        $name = explode('.', $name, 3);
+
+        if (count($name) == 2) {
+            unset($this->config[strtolower($name[0])][$name[1]]);
+        } else {
+            unset($this->config[strtolower($name[0])][$name[1]][$name[2]]);
         }
     }
 
@@ -202,6 +208,23 @@ class Config {
      */
     public function __isset($name) {
         return $this->has($name);
+    }
+
+    // ArrayAccess
+    public function offsetSet($name, $value) {
+        $this->set($name, $value);
+    }
+
+    public function offsetExists($name) {
+        return $this->has($name);
+    }
+
+    public function offsetUnset($name) {
+        $this->remove($name);
+    }
+
+    public function offsetGet($name) {
+        return $this->get($name);
     }
 
 }
